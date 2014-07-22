63c35e3c2a2873b1b7757f9696056d765ee6a557
pinzolo <pinzolo@gmail.com>
2014-07-22 15:26:46 +0900

    Change indices of mx_taggings to be simply

--- db/migrate/014_create_mx_taggings.rb
+++ db/migrate/014_create_mx_taggings.rb
@@ -3,11 +3,11 @@ class CreateMxTaggings < ActiveRecord::Migration
     create_table :mx_taggings do |t|
       t.timestamps
 
-      t.integer :tag_id, null: false
       t.references :mx_taggable, polymorphic: true, null: false
+      t.integer :tag_id, null: false
     end
 
-    add_index :mx_taggings, [:tag_id, :mx_taggable_type, :mx_taggable_id], unique: true, name: 'mx_taggings_uk1'
-    add_index :mx_taggings, [:mx_taggable_type, :mx_taggable_id], name: 'mx_taggings_idx1'
+    add_index :mx_taggings, :tag_id
+    add_index :mx_taggings, [:mx_taggable_type, :mx_taggable_id, :tag_id], unique: true, name: 'mx_taggings_uk1'
   end
 end
