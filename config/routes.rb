8330369f7a46d362dac5407909e65d285b5a76ea
pinzolo <pinzolo@gmail.com>
2014-07-22 10:53:06 +0900

    Set basic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,20 @@
-# Plugin's routes
-# See: http://guides.rubyonrails.org/routing.html
+Rails.application.routes.draw do
+  # Global
+  namespace :mx, module: nil do
+    resources :databases, controller: :mx_databases do
+      resources :data_types, controller: :mx_data_types
+    end
+  end
+
+  # Project local
+  resources :projects do
+    get :bx, :controller => :bx_tables, :action => :index
+
+    namespace :mx, module: nil do
+      resources :table_lists, controller: :mx_table_lists do
+        resources :tables, controller: :mx_tables
+        resources :common_column_sets, controller: :mx_common_column_sets
+      end
+    end
+  end
+end
