be35ce1a7407f267045152a1785c30e289a93354
pinzolo <pinzolo@gmail.com>
2014-07-23 12:31:31 +0900

    Add basic permission and project menu

--- init.rb
+++ init.rb
@@ -1,8 +1,21 @@
 Redmine::Plugin.register :redmine_mx do
-  name 'Redmine Mx plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
+  name 'Redmine MX'
+  author 'pinzolo'
+  description 'redmine_mx is a plugin for Redmine'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/pinzolo/redmine_mx'
+  author_url 'https://github.com/pinzolo'
+
+  project_module :mx do
+    permission :view_mx_tables, mx_tables: [:index, :show],
+                                mx_table_lists: [:index, :show],
+                                mx_common_column_sets: [:show],
+                                mx_table_versions: [:index, :show]
+    permission :manage_mx_tables, mx_tables: [:new, :edit, :create, :update, :destroy],
+                                  mx_table_lists: [:new, :edit, :create, :update, :destroy],
+                                  mx_common_column_sets: [:new, :edit, :create, :update, :destroy],
+                                  require: :member
+  end
+
+  menu :project_menu, :mx, { controller: :mx_table_lists, action: :index}, param: :project_id, caption: :'mx.label_mx'
 end
