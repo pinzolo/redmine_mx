625a258867bedc3ac9c5d3f69fb6b4f7a81faaa9
pinzolo <pinzolo@gmail.com>
2014-07-29 23:04:07 +0900

    Add butons (add, duplicate, delete)

--- assets/javascripts/redmine_mx.js
+++ assets/javascripts/redmine_mx.js
@@ -5,6 +5,18 @@ function randomId() {
 function prepareMxDbmsProductVue(data) {
   return new Vue({
     el: '#mx_dbms_product_form',
-    data: data
+    data: data,
+    methods: {
+      addDataType: function() {
+        this.$data.data_types.push({ id: randomId(), name: '', sizable: false, scalable: false, use_by_default: false });
+      },
+      removeDataType: function(dataType) {
+        this.$data.data_types.$remove(dataType.$index);
+      },
+      duplicateDataType: function(dataType) {
+        var data = dataType.$data.data_type;
+        this.$data.data_types.splice(dataType.$index + 1, 0, { id: randomId(), name: data.name, sizable: data.sizable, scalable: data.scalable, use_by_default: data.use_by_default });
+      }
+    }
   });
 }
