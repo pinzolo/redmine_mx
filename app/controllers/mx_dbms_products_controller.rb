05ce3d2f7f23d658e72f9230a8a57b3c899b48e9
pinzolo <pinzolo@gmail.com>
2014-07-30 14:24:04 +0900

    Allow not admin user to access dbms_products#index and dbms_products#show page

--- app/controllers/mx_dbms_products_controller.rb
+++ app/controllers/mx_dbms_products_controller.rb
@@ -1,20 +1,22 @@
 class MxDbmsProductsController < ApplicationController
   unloadable
-  layout 'admin'
 
-  before_filter :require_admin
+  before_filter :require_admin, only: [:new, :create, :edit, :update]
   before_filter :find_dbms_product, only: [:show, :edit, :update, :destroy]
 
   def index
     @dbms_products = MxDbmsProduct.order(:name).to_a
+    render layout: User.current.admin? ? 'admin' : 'base'
   end
 
   def show
+    render layout: User.current.admin? ? 'admin' : 'base'
   end
 
   def new
     @dbms_product = MxDbmsProduct.new
     @vue_model = MxDbmsProductVueModel.new(@dbms_product)
+    render layout: 'admin'
   end
 
   def create
@@ -22,6 +24,7 @@ class MxDbmsProductsController < ApplicationController
 
   def edit
     @vue_model = MxDbmsProductVueModel.new(@dbms_product)
+    render layout: 'admin'
   end
 
   def update
