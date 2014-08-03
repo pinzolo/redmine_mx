6e617adac6d568820d1234cdaa17f62c947cdc0d
pinzolo <pinzolo@gmail.com>
2014-08-03 10:45:47 +0900

    Coveralls

---
+++ Gemfile
@@ -0,0 +1,4 @@
+group :test do
+  gem "coveralls"
+  gem "minitest-excludes"
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,13 @@
+require 'coveralls'
+require 'simplecov'
+
+SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+SimpleCov.start do
+  add_filter do |source_file|
+    !source_file.filename.include?('plugins/redmine_mx') || !source_file.filename.end_with?('.rb')
+  end
+end
+
 # Load the Redmine helper
 require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
 
