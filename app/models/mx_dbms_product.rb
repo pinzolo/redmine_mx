9021db0897b88379f204c7aded11fc8d759452f7
pinzolo <pinzolo@gmail.com>
2014-08-13 12:10:19 +0900

    Update variable (vm -> vue_model)

--- app/models/mx_dbms_product.rb
+++ app/models/mx_dbms_product.rb
@@ -20,31 +20,31 @@ class MxDbmsProduct < ActiveRecord::Base
     PRODUCT_TYPES[type]
   end
 
-  def save_with!(vm)
+  def save_with!(vue_model)
     ActiveRecord::Base.transaction do
       if self.persisted?
-        update_with!(vm)
+        update_with!(vue_model)
       else
-        create_with!(vm)
+        create_with!(vue_model)
       end
     end
   end
 
   private
 
-  def create_with!(vm)
-    self.attributes = vm.params_with(:name, :comment)
+  def create_with!(vue_model)
+    self.attributes = vue_model.params_with(:name, :comment)
     self.save!
-    vm.data_types.each do |vm_data_type|
+    vue_model.data_types.each do |vm_data_type|
       self.data_types.build(vm_data_type.params_with(:name, :sizable, :scalable, :use_by_default)).save!
     end
   end
 
-  def update_with!(vm)
-    self.update_attributes!(vm.params_with(:name, :comment, :lock_version))
-    self.update_attribute(:type, vm.type)
+  def update_with!(vue_model)
+    self.update_attributes!(vue_model.params_with(:name, :comment, :lock_version))
+    self.update_attribute(:type, vue_model.type)
     base_data_types = Hash[self.data_types.map { |data_type| [data_type.name, data_type] }]
-    vm.data_types.each do |vm_data_type|
+    vue_model.data_types.each do |vm_data_type|
       base_data_type = base_data_types.delete(vm_data_type.name)
       data_type_params = vm_data_type.params_with(:name, :sizable, :scalable, :use_by_default)
       if base_data_type
