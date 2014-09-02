3df3f4f1054be050fd0e7b4883b85c06d6fc9771
pinzolo <pinzolo@gmail.com>
2014-09-02 17:25:11 +0900

    Implements vue models for index temporarily

---
+++ app/models/mx_vm/index.rb
@@ -0,0 +1,62 @@
+class MxVm::Index
+  include MxVm::VueModel
+
+  attr_accessor :name, :unique, :condition, :comment, :columns, :column_ids
+  attr_accessor :used_index_names, :belonging_column_ids
+
+  validates :name, presence: true,
+                   length: { maximum: 255 },
+                   exclusion: { in: ->(record){ record.used_index_names }, message: :taken, if: 'name.present?' }
+  validates :condition, length: { maximum: 255 }
+
+  def initialize(params={})
+    if params.is_a?(Hash)
+      build_from_hash(params)
+    elsif params.is_a?(MxIndex)
+      build_from_mx_index(params)
+    end
+    self.columns ||= []
+    self.column_ids = columns.map { |col| col.column_id.to_s }
+  end
+
+  def valid_with_columns?
+    valid_without_columns?
+    assign_values_to_columns_for_validation
+    merge_children_errors!(columns, :column)
+    clear_assigned_values_to_columns
+    errors.empty?
+  end
+  alias_method_chain :valid?, :columns
+
+  private
+
+  def build_from_hash(params)
+    simple_load_values_from_hash!(params, :name)
+    if params[:columns]
+      column_vms = params[:columns].map { |column_id, position| MxVm::IndexColumn.new(column_id: column_id, position: position) }
+      self.columns = column_vms.sort_by { |vm| vm.position.to_i }
+    end
+  end
+
+  def build_from_mx_index(index)
+    simple_load_values_from_object!(index, :name, :condition, :comment)
+    self.columns = index.columns_rels.map { |index_column| MxVm::IndexColumn.new(index_column) }
+  end
+
+  def assign_values_to_columns_for_validation
+    valid_positions = [*(1..columns.size)].map(&:to_s)
+    self.columns.each do |column|
+      column.belonging_column_ids = belonging_column_ids
+      column.valid_positions = valid_positions
+      column.other_positions = self.columns.reject { |col| col.column_id == column.column_id }.map(&:position)
+    end
+  end
+
+  def clear_assigned_values_to_columns
+    self.columns.each do |column|
+      column.belonging_column_ids = nil
+      column.valid_positions = nil
+      column.other_positions = nil
+    end
+  end
+end
--- app/models/mx_vm/table.rb
+++ app/models/mx_vm/table.rb
@@ -1,7 +1,7 @@
 class MxVm::Table
   include MxVm::VueModel
   attr_accessor :physical_name, :logical_name, :database_id, :column_set_id, :table_columns, :comment, :lock_version
-  attr_accessor :primary_key
+  attr_accessor :primary_key, :indices
   attr_accessor :data_types, :column_sets
 
   validates :physical_name, presence: true, length: { maximum: 255 }, mx_db_absence: { class_name: 'MxTable', scope: :database_id }
@@ -41,10 +41,11 @@ class MxVm::Table
 
   def valid_with_children?
     valid_without_children?
-    assign_values_to_columns_for_validation
+    assign_values_for_validation
     merge_children_errors!(table_columns, :column)
     merge_children_errors!([primary_key], :primary_key)
-    clear_assigned_values_to_columns
+    merge_children_errors!(indices, :index)
+    clear_assigned_values
     errors.empty?
   end
   alias_method_chain :valid?, :children
@@ -55,39 +56,76 @@ class MxVm::Table
     simple_load_values_from_hash!(params, :id, :physical_name, :logical_name, :database_id, :column_set_id, :comment, :lock_version)
     self.table_columns = params[:table_columns].values.map { |column_params| MxVm::Column.new(column_params) } if params[:table_columns]
     self.primary_key = MxVm::PrimaryKey.new(params[:primary_key])
+    self.indices = params[:indices].values.map { |index_params| MxVm::Index.new(index_params) } if params[:indices]
   end
 
   def build_from_mx_table(table)
     simple_load_values_from_object!(table, :id, :physical_name, :logical_name, :database_id, :column_set_id, :comment, :lock_version)
     self.table_columns = table.table_columns.map { |column| MxVm::Column.new(column) }
     self.primary_key = MxVm::PrimaryKey.new(table.primary_key)
+    self.indices = table.indices.map { |index| MxVm::Index.new(index) }
   end
 
   def safe_collections
     self.table_columns ||= []
     self.data_types ||= []
     self.column_sets ||= []
+    self.indices ||= []
   end
 
-  def assign_values_to_columns_for_validation
+  def assign_values_for_validation
+    assign_values_for_columns_validation
+
+    other_tables = MxDatabase.find(database_id).tables.reject { |table| table.id.to_s == self.id.to_s }
+    belonging_column_ids = columns.map { |column| column.id.to_s }
+
+    assign_values_for_primary_key_validation(other_tables, belonging_column_ids)
+    assign_values_for_indices_validation(other_tables, belonging_column_ids)
+  end
+
+  def assign_values_for_columns_validation
     data_type_ids = data_types.map { |data_type| data_type.id.to_s }
     table_columns.each do |column|
       column.data_type_ids = data_type_ids
       column.using_physical_names = self.columns.reject { |col| col.id == column.id }.map(&:physical_name)
     end
-    primary_key_names_in_database = MxDatabase.find(database_id).tables.map do |table|
-      table.primary_key.try(:name).presence if table.id.to_s != self.id.to_s
-    end
-    primary_key.used_primary_key_names = primary_key_names_in_database.compact
-    primary_key.belonging_column_ids = columns.map { |column| column.id.to_s }
   end
 
-  def clear_assigned_values_to_columns
+  def assign_values_for_primary_key_validation(other_tables, belonging_column_ids)
+    primary_key.used_primary_key_names = other_tables.map { |table| table.primary_key.try(:name).presence }.compact
+    primary_key.belonging_column_ids = belonging_column_ids
+  end
+
+  def assign_values_for_indices_validation(other_tables, belonging_column_ids)
+    other_index_names = other_tables.map { |table| table.indices.map(&:name) }.flatten
+    indices.each do |index|
+      index.used_index_names = other_index_names
+      index.belonging_column_ids = belonging_column_ids
+    end
+  end
+
+  def clear_assigned_values
+    clear_assigned_values_of_columns
+    clear_assigned_values_of_primary_keys
+    clear_assigned_values_of_indices
+  end
+
+  def clear_assigned_values_of_columns
     table_columns.each do |column|
       column.data_type_ids = nil
       column.using_physical_names = nil
     end
+  end
+
+  def clear_assigned_values_of_primary_keys
     primary_key.used_primary_key_names = nil
     primary_key.belonging_column_ids = nil
+  end
+
+  def clear_assigned_values_of_indices
+    indices.each do |index|
+      index.used_index_names = nil
+      index.belonging_column_ids = nil
+    end
   end
 end
