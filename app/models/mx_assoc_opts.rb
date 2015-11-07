878d8b5ce65cac343cc9d0c3a2a9223fe27b3009
pinzolo <pinzolo@gmail.com>
2015-11-07 13:51:56 +0900

    Invalid operator

--- app/models/mx_assoc_opts.rb
+++ app/models/mx_assoc_opts.rb
@@ -3,18 +3,16 @@ module MxAssocOpts
 
   module ClassMethods
     def assoc_opts(options)
-      if Gem::Version.new(Rails.version) > Gem::Version.new('4.0.0')
-        order_option = options.delete(:order)
-        include_option = options.delete(:include)
-        if order_option && include_option
-          [Proc.new { order(order_option).includes(include_option) }, options]
-        elsif order_option
-          [Proc.new { order(order_option) }, options]
-        elsif include_option
-          [Proc.new { includes(include_option) }, options]
-        else
-          [options]
-        end
+      return [options] if Gem::Version.new(Rails.version) < Gem::Version.new('4.0.0')
+
+      order_option = options.delete(:order)
+      include_option = options.delete(:include)
+      if order_option && include_option
+        [Proc.new { order(order_option).includes(include_option) }, options]
+      elsif order_option
+        [Proc.new { order(order_option) }, options]
+      elsif include_option
+        [Proc.new { includes(include_option) }, options]
       else
         [options]
       end
