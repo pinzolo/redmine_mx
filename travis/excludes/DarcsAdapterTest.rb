f4ac177faea71e3df359913b9da98334af8c9ef0
pinzolo <pinzolo@gmail.com>
2015-11-07 12:07:54 +0900

    Update CI test env

--- travis/excludes/DarcsAdapterTest.rb
+++
@@ -1,1 +0,0 @@
-exclude :test_revisions, 'not work on current darcs version' if Redmine::VERSION::STRING < '2.5.0'
--- travis/excludes/RepositoriesSubversionControllerTest.rb
+++
@@ -1,1 +0,0 @@
-exclude :test_revision_diff_raw_format, 'Broken on Redmine 2.3.x' if Redmine::VERSION::STRING < '2.4.0'
